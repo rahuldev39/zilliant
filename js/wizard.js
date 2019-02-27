07e16b3fc2d1ee027565a6b9344a25b9c80434c8
rahuldev39 <rahuldev.w@gmail.com>
2019-02-27 14:03:41 +0530

    minor changes wizard

--- js/wizard.js
+++ js/wizard.js
@@ -33,36 +33,3 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
-
-
-//according menu
-
-$(document).ready(function()
-{
-    //Add Inactive Class To All Accordion Headers
-    $('.accordion-header').toggleClass('inactive-header');
-
-	//Set The Accordion Content Width
-	var contentwidth = $('.accordion-header').width();
-	$('.accordion-content').css({});
-
-	//Open The First Accordion Section When Page Loads
-	$('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
-	$('.accordion-content').first().slideDown().toggleClass('open-content');
-
-	// The Accordion Effect
-	$('.accordion-header').click(function () {
-		if($(this).is('.inactive-header')) {
-			$('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
-			$(this).toggleClass('active-header').toggleClass('inactive-header');
-			$(this).next().slideToggle().toggleClass('open-content');
-		}
-
-		else {
-			$(this).toggleClass('active-header').toggleClass('inactive-header');
-			$(this).next().slideToggle().toggleClass('open-content');
-		}
-	});
-
-	return false;
-});
